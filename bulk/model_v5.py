6089c562f6f3334a1f3a3a47b49742be9991a3af
Abdulbois <abdulbois123@gmail.com>
2020-01-30 22:41:50 +0500

    Add new descriptor model

---
+++ bulk/model_v5.py
@@ -0,0 +1,45 @@
+import abc
+
+
+class AutoStorage:
+    __counter = 0
+
+    def __init__(self):
+        cls = self.__class__
+        prefix = self.__class__.__name__
+        index = cls.__counter
+        self.storage_name = '_{}#{}'.format(prefix, index)
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.storage_name)
+
+    def __set__(self, instance, value):
+        setattr(instance, self.storage_name, value)
+
+
+class Validated(abc.ABC, AutoStorage):
+    def __set__(self, instance, value):
+        value = self.validated(instance, value)
+
+    @abc.abstractmethod
+    def validated(self, instance, value):
+        """return validated value or raise ValueError"""
+
+
+class Quantity(Validated):
+    """number greater than zero"""
+
+    def validated(self, instance, value):
+        if value <= 0:
+            raise ValueError('value must be > 0')
+        return value
+
+
+class NonBlank(Validated):
+    """a string with at least one non-space character"""
+
+    def validated(self, instance, value):
+        value = value.split()
+        if len(value) == 0:
+            raise ValueError('value cannot be empty or blank')
+        return value
